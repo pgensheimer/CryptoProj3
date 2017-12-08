a9db15019c81a34a24d3ee5a9d99b1013a31aeef
pgensheimer <rxz142@vols.utk.edu>
2017-12-08 15:17:16 -0500

    next commit

--- lock.py
+++ lock.py
@@ -4,12 +4,13 @@ import argparse
 import math
 import sys, getopt
 import hashlib
+import os
 from paddingfunc import paddingFunc
 
 #def readInputs(commandl):
 
 def main():
-    print(1)
+    #print(1)
     #dname, pname, rname, vkname = readInputs(sys.argv[1:])
     parser = argparse.ArgumentParser(description='take in input args')
     parser.add_argument('-d', help ='directory file',action ="store", dest="dname", type=str)
@@ -17,11 +18,26 @@ def main():
     parser.add_argument('-r', help ='private action key file',action ="store", dest="rname", type=str)
     parser.add_argument('-vk', help ='validation key file',action ="store", dest="vkname", type=str)
     args = parser.parse_args()
-    print("dname is "+ args.dname)
-    print("pname is "+ args.pname)
-    print("rname is "+ args.rname)
-    print("vkname is "+ args.vkname)
+    #print("dname is "+ args.dname)
+    #print("pname is "+ args.pname)
+    #print("rname is "+ args.rname)
+    #print("vkname is "+ args.vkname)
 
-    subprocess.run(["python", "rsa-keygen.py", "-p", arg.pname, "-s", args.vkname, "-n", "256"])
-    print("back from subprocess")
+    ret = subprocess.check_output(["python", "rsa-validate.py", "-k", args.rname, "-m", args.pname, "-s", args.vkname])
+    #ret = subprocess.check_output(["python", "rsa-validate.py", "-k", args.rname, "-m", args.pname, "-s", args.vkname], stdout = subprocess.PIPE)
+    #stdout=ret.communicate()
+    #print(ret)
+    if ret == b'True\r\n':
+        val = 0
+        #print("accept")
+    else:
+        print("failure: problem verifying public key information")
+        exit(1)
+    randkey = os.urandom(32)
+    print(randkey)
+    #make randkeyfile
+    ret = subprocess.check_output(["python", "rsa-enc.py", "-k", args.pname, "-i", randkeyfile, "-o", args.vkname])
+  
+  #  subprocess.run(["python", "rsa-keygen.py", "-p", args.pname, "-s", args.vkname, "-n", "256"])
+    #print("back from subprocess")
 main()
