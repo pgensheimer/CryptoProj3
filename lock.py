ef0e11d975cbb4f9900bc87524ab148491784f98
pgensheimer <rxz142@vols.utk.edu>
2017-12-08 14:00:38 -0500

    added parsing to unlock and lock

---
+++ lock.py
@@ -0,0 +1,21 @@
+import argparse
+import math
+import sys, getopt
+import hashlib
+from paddingfunc import paddingFunc
+
+#def readInputs(commandl):
+
+def main():
+    #dname, pname, rname, vkname = readInputs(sys.argv[1:])
+    parser = argparse.ArgumentParser(description='take in input args')
+    parser.add_argument('-d', help ='directory file',action ="store", dest="dname", type=str)
+    parser.add_argument('-p', help ='action public key file',action ="store", dest="pname", type=str)
+    parser.add_argument('-r', help ='private action key file',action ="store", dest="rname", type=str)
+    parser.add_argument('-vk', help ='validation key file',action ="store", dest="vkname", type=str)
+    args = parser.parse_args()
+    print("dname is "+ args.dname)
+    print("pname is "+ args.pname)
+    print("rname is "+ args.rname)
+    print("vkname is "+ args.vkname)
+main()
